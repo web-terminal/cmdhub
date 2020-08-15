b402123227d3686137bcb1f477a8a2e91b7bdc83
junniex <820576704@qq.com>
2020-08-15 15:18:14 +0800

    update main.js

--- github/main.js
+++ github/main.js
@@ -3,20 +3,9 @@ var githubCmd = function() {
     this.subCmds = {
         ls: {
             desc: "show repos.",
-            exec: function(command, cmdwin) {
+            Exec: function(command, cmdwin) {
                 console.log('github ls command.');
             }
-        }
-    };
-    this.Exec = function(command, terminal) {
-        if (command.content.length > 0) {
-            if (this.subCmds.hasOwnProperty(command.content[0])) {
-                this.subCmds[command.content[0]].exec(command, terminal);
-            } else {
-                terminal.displayErrorOutput("invalid command "+command.content[0]);
-            }
-        } else {
-            terminal.displayErrorOutput("please input sub command. like: github ls <first content>");
         }
     }
 }
